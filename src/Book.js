a44f902ea4a401011c0f91ab574da1ab0f24d37a
Natalia-Ushakova <n.n.ushakova@gmail.com>
2018-11-11 16:18:55 -0500

    Add component Book

---
+++ src/Book.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Book extends Component {
+    // PROPTYPES TO CHANGE
+    /*static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+      }*/
+
+    render() {
+       return(
+        <li>
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
+                    <div className="book-shelf-changer">
+                        <select value={props.book.shelf} onChange={(event)=>{props.onChangeShelf(event, props.book)}}>                
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">{props.book.title}</div>
+                <div className="book-authors">{props.book.author}</div>
+            </div>
+        </li>
+       )
+    }
+      
+}
+
+export default Book;
+
