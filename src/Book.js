3f3a4421f008bef4eb44a45159f86ffd72132889
Natalia-Ushakova <n.n.ushakova@gmail.com>
2018-11-12 19:35:40 -0500

    Remove starting content

--- src/Book.js
+++ src/Book.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 class Book extends Component {
     // PROPTYPES TO CHANGE
@@ -10,12 +10,12 @@ class Book extends Component {
 
     render() {
        return(
-        <li>
+        <li> {/*probably li to be removed*/}
             <div className="book">
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
                     <div className="book-shelf-changer">
-                        <select value={props.book.shelf} onChange={(event)=>{props.onChangeShelf(event, props.book)}}>                
+                        <select /*value={props.book.shelf} onChange={(event)=>{props.onChangeShelf(event, props.book)}}*/>                
                             <option value="move" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
@@ -24,8 +24,8 @@ class Book extends Component {
                         </select>
                     </div>
                 </div>
-                <div className="book-title">{props.book.title}</div>
-                <div className="book-authors">{props.book.author}</div>
+                <div className="book-title">title goes here</div>
+                <div className="book-authors">props.book.author</div>
             </div>
         </li>
        )
